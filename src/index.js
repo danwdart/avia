f4b40a3fd88cf720496414a15e3b5746b6d02bab
Dan <git@dandart.co.uk>
2017-03-26 21:39:02 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import Twitter from 'twitter';
+import objConfig from '../config';
+
+const client = new Twitter(objConfig.client),
+    pRt = id => client.post(
+        `statuses/retweet/${id}`
+    ),
+    pQuote = (tweet, strText) => client.post(
+        `statuses/update`,
+        {
+            status: `${strText} https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
+        }
+    ),
+    pStream = strAction => client.stream(
+        `statuses/filter`,
+        {
+            track: strAction
+        }
+    ),
+    pDelay = ms => new Promise((res, rej) => setTimeout(res, ms)),
+    pListenAndRt = async (strAction, strText) => {
+        console.log(`Listening for ${strAction}`);
+        try {
+            let stream = await pStream(strAction);
+
+            stream.on(`data`, async tweet => await pQuote(tweet, strText));
+            stream.on(`error`, err => console.log(err));
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    main = async () => {
+        for (let strAction in objConfig.actions) {
+            let strText = objConfig.actions[strAction];
+            try {
+                await pListenAndRt(strAction, strText);
+                await pDelay(1000);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    };
+
+main();
