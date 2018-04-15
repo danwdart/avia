ecd68a95cefcc99e760babb7fd69f6e6e418ce23
Dan Dart <git@dandart.co.uk>
2018-04-15 21:56:32 +0100

    update babel and introduce delete endpoint

---
+++ src/delete.js
@@ -0,0 +1,28 @@
+import Twitter from 'twitter';
+import objConfig from '../config';
+
+const client = new Twitter(objConfig.client),
+    main = async () => {
+        try {
+            const myTweets = await client.get(
+                `statuses/user_timeline`,
+                {
+                    count: 200,
+                    screen_name: objConfig.username
+                }
+            );
+            console.log(myTweets);
+
+            for (const {id_str} of myTweets) {
+                console.log(`Deleting tweet ${id_str}`)
+                await client.post(
+                    `statuses/destroy/${id_str}`,
+                    {}
+                )
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+main();
